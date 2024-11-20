a0f101bafe14ba7b4ebba1b8c197c0bb6975fcd4
Konstantinos Kagkelidis <kaggis@gmail.com>
2024-11-20 23:38:19 +0200

    CAT-587 Show Criteria Metrics in Motivation

--- src/types/criterion.ts
+++ src/types/criterion.ts
@@ -1,3 +1,4 @@
+import { AssessmentTest } from "./assessment";
 import { ResponsePage } from "./common";
 import { MotivationReference } from "./motivation";
 import { Principle } from "./principle";
@@ -22,5 +23,36 @@ export interface CriterionImperative {
   imp: string;
   label: string;
 }
+
+export interface CriterionType {
+  id: string;
+  label: string;
+  description: string;
+  last_touch: string;
+  url: string;
+  populated_by: string | null;
+}
+
+export interface CriterionInput {
+  cri: string;
+  label: string;
+  description: string;
+  imperative: string;
+  type_criterion_id: string;
+}
+
+export interface CriterionMetric {
+  id: string;
+  name: string;
+  type: string;
+  benchmark_value: number;
+  label_algorithm_type: string;
+  label_type_metric: string;
+  tests: AssessmentTest[];
+}
+
+export type CriterionMetricResponse = {
+  metric: CriterionMetric;
+};
 
 export type CriterionResponse = ResponsePage<Criterion[]>;

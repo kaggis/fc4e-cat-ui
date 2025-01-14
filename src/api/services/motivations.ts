c8edb388118410a4789386fbcb2c57272e0f7fa7
Konstantinos Kagkelidis <kaggis@gmail.com>
2025-01-14 14:07:10 +0200

    CAT-721 Update create-metric backend path. Update create test modal to clear values when reappears

--- src/api/services/motivations.ts
+++ src/api/services/motivations.ts
@@ -611,7 +611,7 @@ export const useCreateMotivationMetric = (
   return useMutation(
     async () => {
       const response = await APIClient(token).post<MetricResponse>(
-        `/v1/registry/motivations/${mtvId}/metric-definition`,
+        `/v1/registry/motivations/${mtvId}/metric`,
         {
           mtr,
           label,
